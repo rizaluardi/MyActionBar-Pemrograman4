dafe85d78a120edf7378e31b70258eab71fc2d40
rizaluardi <rizaluardi@gmail.com>
2021-12-21 14:23:59 +0700

    Rizaluardi
    Rizaluardi

---
+++ app/src/main/java/com/appsmor/rizalmyactionbar/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.appsmor.rizalmyactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
